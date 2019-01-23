01d6cee4009f6d6b5fdb6826b45f2108ff384d04
Angelica Mansala <Wfang9514@gmail.com>
2019-01-23 10:43:08 -0500

    Updates to javadoc

--- rank.java
+++ rank.java
@@ -1,25 +1,67 @@
-// Game of the Generals [Demo]
-// Created by: Angelica C. F. Manansala, Ivan Lau, Caitlin Kwan
-// Created on: January 8, 2018
-// Last Updated: January 19, 2019
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+/** 
+ * Game of the Generals
+ * ICS 4U1 Final CPT
+ * This is a computer version of "Game of the Generals" using concepts and tools learned throughout the course
+ * It enables players to play over a network through the use of Cadawas' SuperSocketMaster
+ * @author Angelica Manansala, Ivan Lau, Caitlin Kwan
+ * @since 2019-01-08
+ * Last Updated: January 23, 2019
+ */
 
 public class rank{
 
 	//properties
 	//rank
-	int intRank;
-	int intX;
-	int intY;
-	boolean blnAlive = true;
-	boolean blnGameEnd = false;
-	int intArrayX;
-	int intArrayY;
-	String strSide;
 	
+	/**
+	 * This represents the rank of the piece
+	 */
+	 
+	int intRank;
+	
+	/**
+	 * This represents the x-coordinate of the piece 
+	 */
+	
+	int intX;
+	
+	/**
+	 * This represents the y-coordinate of the piece 
+	 */
+	
+	int intY;
+	
+	/**
+	 * This indicates whether the piece has been eliminated or not
+	 */
+	
+	boolean blnAlive = true;
+	
+	/**
+	 *  This represents the x-coordinate of the piece on the array, itself
+	 */
+		
+	int intArrayX;
+	
+	/**
+	 *  This represents the x-coordinate of the piece on the array, itself
+	 */
+	
+	int intArrayY;
+		
+	/**
+	 *  This represents the y-coordinate of the piece on the array, itself
+	 */
+	 	
+	String strSide;
+		
+	/**
+	 *  This indicates what side the piece is on (White or Black)
+	 */
 	
 	//methods
 	
